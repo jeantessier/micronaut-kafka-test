8bc4822a7a5ecca42da1a854c16ded045fcad59c
Jean Tessier <jean@jeantessier.com>
2021-02-19 20:12:11 -0800

    Initial producer
    Writes events to the `micronaut.kafka.test.events` topic.

---
+++ producer-app/src/main/groovy/producer/app/controller/HelloController.groovy
@@ -0,0 +1,26 @@
+package producer.app.controller
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+import javax.inject.*
+
+import producer.app.event.Event
+import producer.app.event.EventProducer
+
+@Controller("/")
+class HelloController {
+
+    @Inject
+    EventProducer eventProducer
+
+    @Get(produces = MediaType.TEXT_PLAIN)
+    def index(Optional<String> name) {
+        def message = name.present ? "Hello, ${name.get()}!" : "Hello, world!"
+        eventProducer.send(Event.name, new Event(message: message, date: new Date()))
+
+        return "OK"
+    }
+
+}
