e2c4115905818418e428dbc3452c5b80b2b7b929
Jean Tessier <jean@jeantessier.com>
2021-05-20 21:06:30 -0700

    Split consumers into separate classes

--- consumer-app/src/main/groovy/consumer/app/consumer/StringConsumer.groovy
+++ consumer-app/src/main/groovy/consumer/app/consumer/StringConsumer.groovy
@@ -5,19 +5,13 @@ import io.micronaut.configuration.kafka.annotation.KafkaListener
 import io.micronaut.configuration.kafka.annotation.OffsetReset
 import io.micronaut.configuration.kafka.annotation.Topic
 
-import java.text.SimpleDateFormat
-
-import consumer.app.event.Event
-
 @KafkaListener(offsetReset = OffsetReset.EARLIEST)
-class EventConsumer {
+class StringConsumer {
 
-    static final dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-
-    @Topic("micronaut.kafka.test.events")
-    def receive(@KafkaKey String key, Event event, long offset, int partition, String topic, long timestamp) {
+    @Topic("micronaut.kafka.test.strings")
+    def receive(@KafkaKey String key, String string, long offset, int partition, String topic, long timestamp) {
         println "${topic}[${partition}] offset: ${offset} timestamp: ${timestamp} key: ${key}"
-        println "[${getClass().name}] ${dateFormat.format(event.date)}: ${event.message}"
+        println "[${getClass().name}] ${string}"
     }
 
 }
